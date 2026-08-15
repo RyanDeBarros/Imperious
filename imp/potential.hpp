144cebb5bcd6595e41994f8eb26e879a5c738f76
RyanDeBarros <ryan2004db@gmail.com>
2026-08-15 17:26:36 -0400

    Add clamp() functions to potential module

--- imp/potential.hpp
+++ imp/potential.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <ostream>
 #include <utility>
 
@@ -48,6 +49,40 @@ namespace imp
 	};
 
 	inline constexpr nullpotential_t nullpotential{};
+
+	// TODO handle min/max values in incorrect order
+	
+	template<typename ty>
+	bool clamp(ty& data, imp::potential<ty> min, imp::potential<ty> max)
+	{
+		const bool below_min = min.has_value && data < min.value;
+		const bool above_max = max.has_value && data > min.value;
+		
+		if (below_min)
+			data = min.value;
+		else if (above_max)
+			data = max.value;
+
+		return below_min || above_max;
+	}
+
+	template<typename ty>
+	bool clamp(ty* data, const size_t count, imp::potential<ty> min, imp::potential<ty> max)
+	{
+		bool changed = false;
+		for (size_t i = 0; i < count; ++i)
+			changed |= clamp(data[i], min, max);
+		return changed;
+	}
+
+	template<typename ty, size_t n>
+	bool clamp(std::array<ty, n>& data, imp::potential<ty> min, imp::potential<ty> max)
+	{
+		bool changed = false;
+		for (size_t i = 0; i < n; ++i)
+			changed |= clamp(data[i], min, max);
+		return changed;
+	}
 
 	template<typename ty>
 	std::ostream& operator<<(std::ostream& os, const potential<ty>& opt)
