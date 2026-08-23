86187d7ce772288b0c79eba7aa813e1658848b14
RyanDeBarros <ryan2004db@gmail.com>
2026-08-23 15:48:10 -0400

    handle_error() functions

---
+++ imp/error_logging.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <utility>
+
+namespace imp
+{
+	template<typename error, typename func, typename logger>
+	void handle_error(func&& func_, logger&& logger_)
+	{
+		try
+		{
+			std::forward<func>(func_)();
+		}
+		catch (const error& e)
+		{
+			std::forward<logger>(logger_)(e);
+		}
+	}
+
+	template<typename error, typename... errors, typename func, typename logger>
+	void handle_error(func&& func_, logger&& logger_)
+	{
+		try
+		{
+			handle_error<errors...>(std::forward<func>(func_), logger_);
+		}
+		catch (const error& e)
+		{
+			logger_(e);
+		}
+	}
+}
