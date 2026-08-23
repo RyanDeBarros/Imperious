69e676d6f074e95f3fea3a951a2db27e6e6a59ff
RyanDeBarros <ryan2004db@gmail.com>
2026-08-23 16:03:54 -0400

    instance_stack & instance_guard

---
+++ imp/instance_guard.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <stdexcept>
+
+namespace imp
+{
+	template<typename derived>
+	struct instance_guard
+	{
+	private:
+		static inline derived* _instance = nullptr;
+
+	public:
+		instance_guard()
+		{
+			if (_instance)
+				throw std::logic_error("existing active instance"); // TODO class name prefix + imp::error ?
+
+			_instance = static_cast<derived*>(this);
+		}
+
+		instance_guard(const instance_guard&) = delete;
+		instance_guard(instance_guard&&) = delete;
+
+		~instance_guard()
+		{
+			if (_instance == static_cast<derived*>(this))
+				_instance = nullptr;
+		}
+
+		static derived& instance()
+		{
+			if (_instance)
+				return *_instance;
+			else
+				throw std::logic_error("no active instance"); // TODO class name prefix + imp::error ?
+		}
+
+		static derived* active_instance()
+		{
+			return _instance;
+		}
+	};
+}
---
+++ imp/instance_stack.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <stack>
+
+namespace imp
+{
+	template<typename derived>
+	struct instance_stack
+	{
+	private:
+		static inline std::stack<derived*> _instances;
+
+	public:
+		instance_stack()
+		{
+			_instances.push(static_cast<derived*>(this));
+		}
+
+		instance_stack(const instance_stack&) = delete;
+		instance_stack(instance_stack&&) = delete;
+
+		~instance_stack()
+		{
+			_instances.pop();
+		}
+
+		static derived& instance()
+		{
+			if (!_instances.empty())
+				return *_instances.top();
+			else
+				throw std::logic_error("no active instance"); // TODO class name prefix + imp::error ?
+		}
+
+		static size_t instance_count()
+		{
+			return _instances.size();
+		}
+	};
+}
