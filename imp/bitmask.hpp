9af6da39078282eea98d789fa66431fc47c02804
RyanDeBarros <ryan2004db@gmail.com>
2026-08-30 19:42:06 -0400

    Fix backslashes in bitmask macros

--- imp/bitmask.hpp
+++ imp/bitmask.hpp
@@ -5,22 +5,22 @@
 #define IMP_IMPLEMENT_BITMASK(enum_ty) \
 	inline enum_ty operator|(enum_ty lhs, enum_ty rhs) \
 	{ \
-		using sub_ty = std::underlying_type_t<enum_ty>;
+		using sub_ty = std::underlying_type_t<enum_ty>; \
 		return static_cast<enum_ty>(static_cast<sub_ty>(lhs) | static_cast<sub_ty>(rhs)); \
 	} \
 	inline enum_ty operator&(enum_ty lhs, enum_ty rhs) \
 	{ \
-		using sub_ty = std::underlying_type_t<enum_ty>;
+		using sub_ty = std::underlying_type_t<enum_ty>; \
 		return static_cast<enum_ty>(static_cast<sub_ty>(lhs) & static_cast<sub_ty>(rhs)); \
 	} \
 	inline enum_ty operator^(enum_ty lhs, enum_ty rhs) \
 	{ \
-		using sub_ty = std::underlying_type_t<enum_ty>;
+		using sub_ty = std::underlying_type_t<enum_ty>; \
 		return static_cast<enum_ty>(static_cast<sub_ty>(lhs) ^ static_cast<sub_ty>(rhs)); \
 	} \
 	inline enum_ty operator~(enum_ty value) \
 	{ \
-		using sub_ty = std::underlying_type_t<enum_ty>;\
+		using sub_ty = std::underlying_type_t<enum_ty>; \
 		return static_cast<enum_ty>(~static_cast<sub_ty>(value)); \
 	} \
 	inline enum_ty& operator|=(enum_ty& lhs, enum_ty rhs) { lhs = lhs | rhs; return lhs; } \
