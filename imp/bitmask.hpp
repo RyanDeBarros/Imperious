4f82f4c5a0a7f51c88954aec47b616c3670b0ded
RyanDeBarros <ryan2004db@gmail.com>
2026-08-30 19:40:58 -0400

    Don't assume enum underlying type is int in bitmask macros

--- imp/bitmask.hpp
+++ imp/bitmask.hpp
@@ -1,19 +1,37 @@
 #pragma once
 
+#include <concepts>
+
 #define IMP_IMPLEMENT_BITMASK(enum_ty) \
-inline enum_ty operator|(enum_ty lhs, enum_ty rhs) { return static_cast<enum_ty>(static_cast<int>(lhs) | static_cast<int>(rhs)); } \
-inline enum_ty operator&(enum_ty lhs, enum_ty rhs) { return static_cast<enum_ty>(static_cast<int>(lhs) & static_cast<int>(rhs)); } \
-inline enum_ty operator^(enum_ty lhs, enum_ty rhs) { return static_cast<enum_ty>(static_cast<int>(lhs) ^ static_cast<int>(rhs)); } \
-inline enum_ty operator~(enum_ty value) {	return static_cast<enum_ty>(~static_cast<int>(value)); } \
-inline enum_ty& operator|=(enum_ty& lhs, enum_ty rhs) { lhs = lhs | rhs; return lhs; } \
-inline enum_ty& operator&=(enum_ty& lhs, enum_ty rhs) { lhs = lhs & rhs; return lhs; } \
-inline enum_ty& operator^=(enum_ty& lhs, enum_ty rhs) { lhs = lhs ^ rhs; return lhs; }
+	inline enum_ty operator|(enum_ty lhs, enum_ty rhs) \
+	{ \
+		using sub_ty = std::underlying_type_t<enum_ty>;
+		return static_cast<enum_ty>(static_cast<sub_ty>(lhs) | static_cast<sub_ty>(rhs)); \
+	} \
+	inline enum_ty operator&(enum_ty lhs, enum_ty rhs) \
+	{ \
+		using sub_ty = std::underlying_type_t<enum_ty>;
+		return static_cast<enum_ty>(static_cast<sub_ty>(lhs) & static_cast<sub_ty>(rhs)); \
+	} \
+	inline enum_ty operator^(enum_ty lhs, enum_ty rhs) \
+	{ \
+		using sub_ty = std::underlying_type_t<enum_ty>;
+		return static_cast<enum_ty>(static_cast<sub_ty>(lhs) ^ static_cast<sub_ty>(rhs)); \
+	} \
+	inline enum_ty operator~(enum_ty value) \
+	{ \
+		using sub_ty = std::underlying_type_t<enum_ty>;\
+		return static_cast<enum_ty>(~static_cast<sub_ty>(value)); \
+	} \
+	inline enum_ty& operator|=(enum_ty& lhs, enum_ty rhs) { lhs = lhs | rhs; return lhs; } \
+	inline enum_ty& operator&=(enum_ty& lhs, enum_ty rhs) { lhs = lhs & rhs; return lhs; } \
+	inline enum_ty& operator^=(enum_ty& lhs, enum_ty rhs) { lhs = lhs ^ rhs; return lhs; }
 
 #define IMP_DECLARE_NESTED_BITMASK(enum_ty) \
-friend enum_ty operator|(enum_ty, enum_ty); \
-friend enum_ty operator&(enum_ty, enum_ty); \
-friend enum_ty operator^(enum_ty, enum_ty); \
-friend enum_ty operator~(enum_ty); \
-friend enum_ty& operator|=(enum_ty&, enum_ty); \
-friend enum_ty& operator&=(enum_ty&, enum_ty); \
-friend enum_ty& operator^=(enum_ty&, enum_ty);
+	friend enum_ty operator|(enum_ty, enum_ty); \
+	friend enum_ty operator&(enum_ty, enum_ty); \
+	friend enum_ty operator^(enum_ty, enum_ty); \
+	friend enum_ty operator~(enum_ty); \
+	friend enum_ty& operator|=(enum_ty&, enum_ty); \
+	friend enum_ty& operator&=(enum_ty&, enum_ty); \
+	friend enum_ty& operator^=(enum_ty&, enum_ty);
