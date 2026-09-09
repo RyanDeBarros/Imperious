c2c1aaa3470e85985b76b743116b0e174b4bddd4
RyanDeBarros <ryan2004db@gmail.com>
2026-09-08 20:31:57 -0400

    imp::instance_tracker

---
+++ imp/instance_tracker.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <unordered_set>
+
+namespace imp
+{
+	template<typename derived>
+	class instance_tracker
+	{
+		std::unordered_set<derived*> _instances;
+
+	public:
+		instance_tracker()
+		{
+			_instances.insert(static_cast<derived*>(this));
+		}
+
+		instance_tracker(const instance_tracker&)
+		{
+			_instances.insert(static_cast<derived*>(this));
+		}
+
+		instance_tracker(instance_tracker&&)
+		{
+			_instances.insert(static_cast<derived*>(this));
+		}
+
+		~instance_tracker()
+		{
+			_instances.erase(static_cast<derived*>(this));
+		}
+
+		static const std::unordered_set<derived*>& instances()
+		{
+			return _instances;
+		}
+	};
+}
