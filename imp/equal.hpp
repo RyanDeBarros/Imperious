e084b91dcd88a90412595f6d91ab19408fa3e956
RyanDeBarros <ryan2004db@gmail.com>
2026-09-08 20:10:49 -0400

    imp::stl_equal

---
+++ imp/equal.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <array>
+
+namespace imp
+{
+	template<typename equal>
+	struct stl_equal
+	{
+		template<typename ty, size_t n>
+		bool operator()(const std::array<ty, n>& a, const std::array<ty, n>& b) const
+		{
+			for (size_t i = 0; i < n; ++i)
+			{
+				if (!equal{}(a[i], b[i]))
+					return false;
+			}
+
+			return true;
+		}
+	};
+}
