6e88cd6d3b57164f2dc93bb8b10e1cc1209cf334
RyanDeBarros <ryan2004db@gmail.com>
2026-09-17 22:24:15 -0400

    imp::soft_singleton derived type must be final

--- imp/soft_singleton.hpp
+++ imp/soft_singleton.hpp
@@ -1,23 +1,28 @@
 #pragma once
 
+#include <type_traits>
+
 namespace imp
 {
-	template<typename ty>
+	template<typename derived>
 	class soft_singleton
 	{
 	protected:
 		soft_singleton() = default;
 		~soft_singleton() = default;
 
-	public:
+    private:
 		soft_singleton(const soft_singleton&) = delete;
 		soft_singleton(soft_singleton&&) = delete;
 		soft_singleton& operator=(const soft_singleton&) = delete;
 		soft_singleton& operator=(soft_singleton&&) = delete;
 
-		static ty& instance()
+	public:
+		static derived& instance()
 		{
-			static ty inst;
+            static_assert(std::is_final_v<derived>, "imp::singleton derived type must be final");
+            // TODO note in documentation recommending that derived class should have a private ctor and make singleton its friend
+			static derived inst;
 			return inst;
 		}
 	};
